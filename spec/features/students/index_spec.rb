8638f3fd3476a63f92f50cae793640448242c5d2
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-15 06:45:32 -0600

    Add students to index page

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 describe "As a visitor" do
   describe "When I visit '/students'" do
     it "I see a list of students with the following information: Name, Age, and House (e.g. 'Name: Casseopia Black, Age: 14, House: Slytherin')" do
-      student_1 = Student.new(name: "Harry",
-                             age: 15,
-                           house: "Gryffindor")
+      student_1 = Student.create!(name: "Harry",
+                                   age: 15,
+                                 house: "Gryffindor")
 
-      student_2 = Student.new(name: "Hermione",
-                               age: 15,
-                             house: "Gryffindor")
-      student_3 = Student.new(name: "Draco",
-                               age: 14,
-                             house: "Slytherin")
+      student_2 = Student.create!(name: "Hermione",
+                                   age: 15,
+                                 house: "Gryffindor")
+      student_3 = Student.create!(name: "Draco",
+                                   age: 14,
+                                 house: "Slytherin")
 
       visit '/students'
 
